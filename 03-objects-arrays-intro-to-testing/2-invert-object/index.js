01094b4446982e215cd5912e985d94d4ea43a19a
Ivan Bondar <iubondar@gmail.com>
2026-01-07 14:33:38 +0300

    Add trimSymbols implementation

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,9 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    return undefined;
+  }
+
   return Object.fromEntries(Object.entries(obj).map((pair) => pair.reverse()));
 }
