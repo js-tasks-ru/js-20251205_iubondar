a7f9b28aaf861ea795f719706f6be595794295d3
Ivan Bondar <iubondar@gmail.com>
2026-01-14 10:34:34 +0300

    Simplify invertObject null checking

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,7 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (typeof obj !== "object" || obj === null) {
+  if (!obj) {
     return undefined;
   }
 
